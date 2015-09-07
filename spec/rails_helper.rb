0fcbf49740e492e3fc95a8f05729a3c9a3122fa3
Daniel Dawson <dpdawson@dpdawson.org>
2015-09-07 22:32:32 +0200

    Reduce DatabaseCleaner redundancy.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,10 +66,10 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.clean_with :truncation
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.start
   end
 
