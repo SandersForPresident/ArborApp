c1484d23dba0bdcefafc0cb28fada48f51ecb4db
Zach Schneider <schneidmaster@gmail.com>
2015-09-06 21:04:09 -0500

    Explicitly set foreign key.

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,6 +3,6 @@ class Team < ActiveRecord::Base
   has_and_belongs_to_many :skills
   has_many :team_users
   has_many :users, through: :team_users
-  belongs_to :parent_team, class_name: 'Team'
+  belongs_to :parent_team, class_name: 'Team', foreign_key: :team_id
   has_many :subteams, class_name: 'Team'
 end
