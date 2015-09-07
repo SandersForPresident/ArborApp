42242cb7292b25f8156dab8f916d077df71c3c4b
Zach Schneider <schneidmaster@gmail.com>
2015-09-06 20:44:19 -0500

    Add account and team associations to team model.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,8 @@
 class Team < ActiveRecord::Base
+  belongs_to :account
   has_and_belongs_to_many :skills
   has_many :team_users
   has_many :users, through: :team_users
+  belongs_to :parent_team, class_name: 'Team'
+  has_many :subteams, class_name: 'Team'
 end
