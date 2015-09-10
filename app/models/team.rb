91dea9e7a01512b5f3f76508d18bba4ebe08b698
Daniel Dawson <dpdawson@dpdawson.org>
2015-09-10 22:11:08 +0200

    Fix has_many through associations on User, Group, and Team.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,5 @@
 class Team < ActiveRecord::Base
+  has_many :group_memberships
   has_many :teams
   has_many :users, through: :group_memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :group_memberships
   has_many :groups, through: :group_memberships
+  has_many :team_memberships
   has_many :teams, through: :team_memberships
 
   class << self
