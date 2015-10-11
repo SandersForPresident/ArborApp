19cbe4ae73073a1a03aaa134a6e1e3239870db46
Rob Cole <robcole@gmail.com>
2015-10-11 13:53:40 -0700

    Fix parameter alignment in application helper
    Align parameters in application helper according
    to Rubocop recommendations.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,10 +6,10 @@ module ApplicationHelper
   def sign_in_link(user)
     if user.present?
       link_to t('application.navigation.sign_out'), logout_path,
-        class: 'navbar-brand'
+              class: 'navbar-brand'
     else
       link_to t('application.navigation.sign_in'), login_path('slack'),
-        class: 'navbar-brand'
+              class: 'navbar-brand'
     end
   end
 end
