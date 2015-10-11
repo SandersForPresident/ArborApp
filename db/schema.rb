a1d1bb1e730bc96869c900a78e4aeab18bcc301d
Rob Cole <robcole@gmail.com>
2015-10-11 00:53:57 -0700

    Add auth_token to User model
    Uses has_secure_token to add auth_token for
    "remember me" support.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915141838) do
+ActiveRecord::Schema.define(version: 20151011034744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,9 @@ ActiveRecord::Schema.define(version: 20150915141838) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "email",      null: false
+    t.string   "auth_token"
   end
+
+  add_index "users", ["auth_token"], name: "index_users_on_auth_token", using: :btree
 
 end
